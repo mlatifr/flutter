9cf7cd4ab46e11cbba119811f0c05c768ee3b2cf
mlatifr <mlatifraldi@gmail.com>
2021-04-16 18:19:40 +0700

    belajar Container + BoxDecoration + borderRadius 2

--- lib/main.dart
+++ lib/main.dart
@@ -6,29 +6,14 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Latihan Container'),
-        ),
-        body: Container(
-          color: Colors.orangeAccent,
-          margin: EdgeInsets.fromLTRB(10, 15, 20, 25),
-          padding: EdgeInsets.only(bottom: 20, top: 20),
-          child: Container(
-            margin: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: <Color>[Colors.cyan, Colors.amberAccent])),
-          ),
-        ),
-      ),
-    );
+    return Container();
   }
 }
