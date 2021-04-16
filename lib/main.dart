9271f520dd6aab12b4c669ee8627763f73cb9c77
mlatifr <mlatifraldi@gmail.com>
2021-04-16 18:59:43 +0700

    belajar statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:html';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,8 +14,45 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  int number = 0;
+  double ukuranBtn = 1;
+  void tekanTombol() {
+    // setState() untuk mengubah tampilan sesaui dengan state yang ada saat ini
+    setState(() {
+      number += 1;
+      ukuranBtn += 10;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Belajar StatefullWidget'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(number.toString()),
+              SizedBox(
+                  width: 50 + ukuranBtn,
+                  height: 50 + ukuranBtn,
+                  child: TextButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.blueAccent)),
+                    onPressed: tekanTombol,
+                    child: Text(
+                      'Tambah Ukuran',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
