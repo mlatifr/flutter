85f19143f5aaf0c187c93b8349ef5715c6222ec4
mlatifr <mlatifraldi@gmail.com>
2021-04-16 20:17:45 +0700

    belajar API

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:html';
 import 'dart:ui';
 
+import 'package:aplikasi_hello_world/post_result_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
@@ -14,41 +15,36 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int number = 0;
-  double ukuranBtn = 1;
-  void tekanTombol() {
-    // setState() untuk mengubah tampilan sesaui dengan state yang ada saat ini
-    setState(() {
-      number += 1;
-      ukuranBtn += 10;
-    });
-  }
+  PostResult postResult = null;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Belajar StatefullWidget'),
+          title: Text('API post request'),
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text(number.toString()),
-              SizedBox(
-                  width: 50 + ukuranBtn,
-                  height: 50 + ukuranBtn,
-                  child: TextButton(
-                    style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all(Colors.blueAccent)),
-                    onPressed: tekanTombol,
-                    child: Text(
-                      'Tambah Ukuran',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ))
+              Text((postResult != null)
+                  ? postResult.id +
+                      ' | ' +
+                      postResult.name +
+                      ' | ' +
+                      postResult.job +
+                      ' | ' +
+                      postResult.createdAt
+                  : 'tidak ada data'),
+              TextButton(
+                  onPressed: () {
+                    PostResult.connectToAPI('Badu', 'Dokter').then((value) {
+                      postResult = value;
+                      setState(() {});
+                    });
+                  },
+                  child: Text('Send Request'))
             ],
           ),
         ),
